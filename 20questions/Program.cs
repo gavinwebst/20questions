5d9bb63b1132b75c191d4f68e65155be8f70f587
Gavin <111817327+gavinwebst@users.noreply.github.com>
2024-11-24 16:16:25 -0500

    added a basic BTNode class

---
+++ 20questions/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+
+
+namespace _20questions
+{
+
+    public class TreeNode {
+        public string question;
+        public TreeNode yesNode;
+        public TreeNode noNode;
+
+        public TreeNode(string question) {
+            this.question = question;
+            this.noNode = null;
+            this.yesNode = null;
+        }
+
+        public void setNoNode(TreeNode node) { 
+            this.noNode = node;
+        }
+
+        public void setYesNode(TreeNode node) {
+            this.yesNode = node; 
+        }
+
+        public TreeNode getYesNode() { 
+            return this.yesNode;
+        }
+
+        public TreeNode getNoNode() {
+            return this.noNode;
+        }
+
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

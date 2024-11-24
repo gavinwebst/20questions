f271ce434e7a70dc3000db24318a667a27c5d821
Olivia Croteau <oliviacroteau667@gmail.com>
2024-11-24 17:25:22 -0500

    added Main() logic

--- 20questions/Program.cs
+++ 20questions/Program.cs
@@ -140,7 +140,42 @@ namespace _20questions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            startNewGame();
+            Console.WriteLine("\nStarting the Game");
+            tree.query(); //play one game
+            while (playAgain())
+            {
+                Console.WriteLine();
+                tree.query(); //play one game
+            }
+        }
+        static bool playAgain()
+        {
+            Console.Write("\nPlay Another Game? ");
+            char inputCharacter = Console.ReadLine().ElementAt(0);
+            inputCharacter = Char.ToLower(inputCharacter);
+            while (inputCharacter != 'y' && inputCharacter != 'n')
+            {
+                Console.WriteLine("Incorrect input please enter again: ");
+                inputCharacter = Console.ReadLine().ElementAt(0);
+                inputCharacter = Char.ToLower(inputCharacter);
+            }
+            if (inputCharacter == 'y')
+                return true;
+            else
+                return false;
+        }
+        static void startNewGame()
+        {
+            Console.WriteLine("No previous knowledge found!");
+            Console.WriteLine("Initializing a new game.\n");
+            Console.WriteLine("Enter a question about an object: ");
+            string question = Console.ReadLine();
+            Console.Write("Enter a guess if the response is Yes: ");
+            string yesGuess = Console.ReadLine();
+            Console.Write("Enter a guess if the response is No: ");
+            string noGuess = Console.ReadLine();
+            tree = new BTTree(question, yesGuess, noGuess);
         }
     }
 }
